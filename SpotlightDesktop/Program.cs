3c6ae13b02698ce41ed23a47ccb389d4b2d509c7
nilesh.tochia <nilesh.tochia@gmail.com>
2016-10-15 12:29:02 +0100

    Initial commit

---
+++ SpotlightDesktop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpotlightDesktop;
+
+namespace SpotlightDesktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spotlightDesktop = new SpotlightDesktop();
+            var spotlightImagePath = spotlightDesktop.GetCurrentSpotlightWallpaperPath();
+            spotlightDesktop.SetWallaper(spotlightImagePath);
+        }
+    }
+}
---
+++ SpotlightDesktop/SpotlightDesktop.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace SpotlightDesktop
+{
+    public class SpotlightDesktop
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SystemParametersInfo(UInt32 uiAction, UInt32 uiParam, String pvParam, UInt32 fWinIni);
+        private static UInt32 SPI_SETDESKWALLPAPER = 20;
+        private static UInt32 SPIF_UPDATEINIFILE = 0x1;
+
+        public string GetCurrentSpotlightWallpaperPath()
+        {
+            var lockScreenKey = Registry.GetValue(@"HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Lock Screen\Creative", "LandscapeAssetPath", null);
+            if (lockScreenKey != null)
+            {
+                return lockScreenKey.ToString();
+
+            }
+            return null;
+        }
+
+        public void SetWallaper(string path)
+        {
+            if (path != null)
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE);
+        }
+    }
+}
